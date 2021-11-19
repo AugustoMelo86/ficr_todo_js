948eed6fc74517e5ed178e10e17efdfa42f0efd6
AugustoMelo86 <augustomelo86@gmail.com>
2021-11-18 21:57:15 -0300

    Acrescentar listas e botoes de novo item

--- js/global.js
+++ js/global.js
@@ -1,9 +1,10 @@
 const cardNovaLista = document.getElementById("cardNovaLista")
 const btnNovaLista = document.getElementById("btnNovaLista")
 const formNovaLista = document.getElementById("novaLista")
+const divListas = document.getElementById('listas')
 
 
-const listas = []
+let listas = []
 
 
 btnNovaLista.addEventListener("click",function() {
@@ -21,10 +22,43 @@ formNovaLista.addEventListener('submit', function(event) {
         inputTitulo.value = ""
         listas.push(novaLista)
         window.localStorage.listas = JSON.stringify(listas)
+        renderList()
     }
-    console.log(listas)
 })
 
 formNovaLista.addEventListener('reset', function(event) {
     cardNovaLista.style.display = "none"
 })
+
+//funcao para adicionar os itens na tela
+function renderList(){
+    //lendo as listas salvas no localstorage
+    listas = JSON.parse(window.localStorage.listas)
+    //limpa a div que recebe os cards das listas
+    divListas.innerHTML = ""
+    //para ca lista no array de lista, cria um novo card
+    listas.map(lista => {
+        //criando um título para o card
+        let h2Tag = document.createElement('H2')
+        //adiciona o titulo
+        h2Tag.innerHTML = lista.titulo
+        //cria botao
+        let btnAddItem = document.createElement('BUTTON')
+        btnAddItem.innerHTML = "novo item"
+        //cria uma div para ser o card da lista
+        let divTag = document.createElement('DIV')
+        //add classe card-lista a div para o css
+        divTag.setAttribute('class','card-lista')
+        //add o h2 a div
+        divTag.appendChild(h2Tag)
+        divTag.appendChild(btnAddItem)
+        //add a div card-lista a div que contem todas as listas na tela
+        divListas.appendChild(divTag)
+        console.log(divTag)
+    })
+}
+
+//verifica se existe algo no localstorage para iniciar
+if (window.localStorage.listas?.length > 0) {
+    renderList()
+}
